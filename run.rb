aea77d0f4accbc1418f230dfa6ef667e059a3f7d
Leo Lee <mail@debugtalk.com>
2016-08-09 15:18:06 +0800

    add support of passing arguments in command line.

--- run.rb
+++ run.rb
@@ -1,8 +1,19 @@
 # filename: ios/run.rb
 
+require 'optparse'
 require_relative 'lib/requires'
 
-app_zip_path = '/Users/Leo/MyProjects/AppiumBooster/ios/app/test.zip'
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: run.rb [options]"
+
+  opts.on("-p APP_PATH", "--app_path", "Specify app path") do |v|
+    options[:app_path] = v
+  end
+
+end.parse!
+
+app_zip_path = options[:app_path]
 $driver = AppiumDriver.new(app_zip_path).driver
 project_root_path = File.dirname(__FILE__)
 testcase_suites = File.join(project_root_path, 'ios', 'testcases', '*.csv')
